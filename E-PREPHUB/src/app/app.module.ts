64bd62d0a66eeaec1ea86de7f90b1c7383507502
Nitin Rawat <nitinr426@gmail.com>
2022-05-01 15:22:26 +0530

    Add Coaching api

--- E-PREPHUB/src/app/app.module.ts
+++ E-PREPHUB/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,14 +8,16 @@ import { RegistrationComponent } from './navbarcomponents/registration/registrat
 import { StudentPortalComponent } from './navbarcomponents/student-portal/student-portal.component';
 import { StudentComponent } from './student/student.component';
 import { StudentHomeComponent } from './student/student-home/student-home.component';
-import { MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import  {MatButtonModule} from '@angular/material/button';
-import { MatDividerModule} from '@angular/material/divider';
-import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoachingComponent } from './coaching/coaching.component';
 import { CoachingHomeComponent } from './coaching/coaching-home/coaching-home.component';
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,8 +27,9 @@ import { CoachingHomeComponent } from './coaching/coaching-home/coaching-home.co
     StudentComponent,
     StudentHomeComponent,
     CoachingHomeComponent,
-    CoachingComponent
-    ],
+    CoachingComponent,
+  ],
+
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -34,9 +38,12 @@ import { CoachingHomeComponent } from './coaching/coaching-home/coaching-home.co
     MatDividerModule,
     MatIconModule,
     MatSidenavModule,
-    MatToolbarModule
+    MatToolbarModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
+
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- E-PREPHUB/src/app/navbarcomponents/registration/registration.component.ts
+++ E-PREPHUB/src/app/navbarcomponents/registration/registration.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.scss']
+  styleUrls: ['./registration.component.scss'],
 })
 export class RegistrationComponent implements OnInit {
-
-  constructor() { }
+  coachingReg!: FormGroup;
+  constructor(private formBuilder: FormBuilder, private api: ApiService) {}
 
   ngOnInit(): void {
+    this.coachingReg = this.formBuilder.group({
+      coachingName: ['', Validators.required],
+      adminName: ['', Validators.required],
+      managerName: ['', Validators.required],
+      email: ['', Validators.required, Validators.email],
+      phoneNumber: ['', Validators.required, Validators.maxLength(10)],
+      address: ['', Validators.required],
+      password: ['', Validators.required],
+    });
   }
 
+  addCoaching() {
+    if (this.coachingReg.valid) {
+      this.api.addCoaching(this.coachingReg.value).subscribe({
+        next: (res) => {
+          alert('User added!');
+          this.coachingReg.reset();
+        },
+        error: () => {
+          alert('Error while adding user');
+        },
+      });
+    }
+  }
 }
